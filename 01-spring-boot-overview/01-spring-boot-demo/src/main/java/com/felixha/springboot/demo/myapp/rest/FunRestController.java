f9c91f2507db8e89b4c7f1ce22b41e97b02c0f1c
felixha <hazelnuts23@gmail.com>
2023-12-21 21:23:35 +0800

    init

---
+++ 01-spring-boot-overview/01-spring-boot-demo/src/main/java/com/felixha/springboot/demo/myapp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.felixha.springboot.demo.myapp.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
---
+++ 01-spring-boot-overview/02-dev-tools-demo/src/main/java/com/felixha/springboot/demo/myapp/MyappApplication.java
@@ -0,0 +1,15 @@
+package com.felixha.springboot.demo.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class MyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappApplication.class, args);
+	}
+
+
+}
